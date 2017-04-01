faef5350a67a232fe3226e9d9890f3e10e5f1bad
6420 Laptop\Toby C <prog4life75@gmail.com>
2017-04-01 11:37:13 -0400

    Added my version of a camera

---
+++ Ben RPG/Assets/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+    public Transform cameraLooksAtThis;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void LateUpdate() {
+        transform.position = cameraLooksAtThis.transform.position;        
+    }//End LateUpdate
+}
