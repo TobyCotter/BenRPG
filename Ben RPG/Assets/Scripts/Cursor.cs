87304b8d1a8e7b2773784bb53172aed430ff527b
6420 Laptop\Toby C <prog4life75@gmail.com>
2017-04-22 09:30:20 -0400

    Added Playermovement script.  Removed cameraRaycaster from Camera arm and placed on main camera.

--- Ben RPG/Assets/Scripts/Cursor.cs
+++ Ben RPG/Assets/Scripts/Cursor.cs
@@ -13,6 +13,6 @@ public class Cursor : MonoBehaviour {
 	
 
 	void Update () {
-		Debug.Log("Layer hit = " + cameraRaycaster.layerHit);
+		//Debug.Log("Layer hit = " + cameraRaycaster.layerHit);
 	}//End Update
 }
