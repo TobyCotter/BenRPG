f2a3dbbaa6d7c06372c1638257af30b407b85115
6420 Laptop\Toby C <prog4life75@gmail.com>
2017-06-03 11:39:26 -0400

    Tutorial 15.  Set enemy prefabs.

---
+++ Ben RPG/Assets/Camera & UI/CursorAffordance.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorAffordance : MonoBehaviour {
+	//Variables
+	[SerializeField] Texture2D walkCursor = null;
+	[SerializeField] Texture2D unknownCursor = null;
+	[SerializeField] Texture2D targetEnemy = null;
+	[SerializeField] Vector2 cursorHotspot = new Vector2(96, 96);
+
+	CameraRaycaster cameraRaycaster;
+
+
+	void Start () {
+		cameraRaycaster = GetComponent<CameraRaycaster>();
+	}//End Start
+	
+
+	void Update () {
+		switch(cameraRaycaster.layerHit){
+			case Layer.Walkable:
+				Cursor.SetCursor(walkCursor, cursorHotspot, CursorMode.Auto);
+				break;
+			case Layer.RaycastEndStop:
+				Cursor.SetCursor(unknownCursor, cursorHotspot, CursorMode.Auto);
+				break;
+			case Layer.Enemy: 
+				Cursor.SetCursor(targetEnemy, cursorHotspot, CursorMode.Auto);
+				break;
+			default:
+				Debug.LogError("Don't know what cursor to show");
+				break;
+		}
+	}//End Update
+}
